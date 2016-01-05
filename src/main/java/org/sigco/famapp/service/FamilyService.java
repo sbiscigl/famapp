165cd9283a07ce75eb9e89767d42338b5a291d86
Sam Bisciglia <sam.bisciglia@uptake.com>
2016-01-04 21:10:42 -0600

    logic for adding people complete

--- src/main/java/org/sigco/famapp/service/FamilyService.java
+++ src/main/java/org/sigco/famapp/service/FamilyService.java
@@ -1,17 +1,15 @@
 package org.sigco.famapp.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.common.collect.Lists;
 import org.sigco.famapp.dto.FamilyDto;
-import org.sigco.famapp.dto.PersonDto;
 import org.sigco.famapp.exception.ConflictException;
 import org.sigco.famapp.exception.NotFoundException;
 import org.sigco.famapp.repository.FamilyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.validation.constraints.NotNull;
 
 @Service
 public class FamilyService implements IFamilyService {
@@ -73,7 +71,12 @@ public class FamilyService implements IFamilyService {
 	/*TODO: THIS LOGIC*/
 	public void updateFamilyMembers(int id, int personId) {
 		FamilyDto familyDto = familyRepository.findOneById(id);
-		familyDto.getMembers().add(personId);
+		List<Integer> members = new ArrayList<>();
+		for (int i : familyDto.getMembers()) {
+			members.add(i);
+		}
+		members.add(personId);
+		familyDto.setMembers(members);
 		familyRepository.save(familyDto);
 	}
 }
